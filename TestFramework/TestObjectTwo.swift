e9f18f1bc56be8ae8c167fe2300a70001763b76a
Steve Sanchez <steve@apargo.com>
2016-04-30 22:16:01 -0700

    le Change
    Signed-off-by: Steve Sanchez <steve@apargo.com>

---
+++ TestFramework/TestObjectTwo.swift
@@ -0,0 +1,15 @@
+//
+//  TestObjectTwo.swift
+//  TestFramework
+//
+//  Created by Steven Sanchez on 4/30/16.
+//  Copyright © 2016 Apargo. All rights reserved.
+//
+
+import UIKit
+
+class TestObjectTwo: NSObject {
+    func testInternal() {
+        print("Test internal use of private class")
+    }
+}
